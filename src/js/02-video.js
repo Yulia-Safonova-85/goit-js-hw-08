909b4b14132f0cd4ea761386c1e9135139a69de2
Yulia-Safonova-85 <109626421+Yulia-Safonova-85@users.noreply.github.com>
2022-12-30 17:52:18 +0200

    update 02_JS

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function ({ seconds }) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  }, 1000)
+);
+
+const currentTime = localStorage.getItem('videoplayer-current-time')
+player.setCurrentTime(currentTime) 
+    
+
+
 
